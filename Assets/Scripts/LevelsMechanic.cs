a4391e9caead9c5b6c2b208c8b06847c06114e2c
Strategne123 <lotigara@yandex.ru>
2023-02-18 17:12:36 +0300

    Don't working level lock / unlock mechanic

---
+++ Assets/Scripts/LevelsMechanic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelsMechanic : MonoBehaviour
+{
+    public int level = 1;
+    public GameObject[] closedLevel;
+
+    private void Start()
+    {
+        level = PlayerPrefs.GetInt("level", level);
+        for (int i = 0; 1 < level; i++)
+        {
+            closedLevel[i].SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour 
 {
+    LevelsMechanic levelsMechanic;
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    public void ExitScene(int level)
+    {
+        if (levelsMechanic.level < level)
+        {
+            levelsMechanic.level = level;
+            PlayerPrefs.SetInt("level", level);
+        }
+
     }
     public void Exit()
     {
