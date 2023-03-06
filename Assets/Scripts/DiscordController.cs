f7b0fab98cf2730ca1fb8c1326b426fe5f8f320f
lotigara <lotigara@yandex.ru>
2023-03-06 23:23:36 +0300

    Almost Working Rich Presence

--- Assets/Scripts/DiscordController.cs
+++ Assets/Scripts/DiscordController.cs
@@ -5,35 +5,48 @@ using Discord;
 
 public class DiscordController : MonoBehaviour
 {
+    
+    public Discord.Discord _discord;
+    public string state;
+    public string imageKey;
+    public string imageText;
+    public bool instance;
 
-	public Discord.Discord discord;
+    public void Start()
+    {
+        _discord =  new Discord.Discord(clientId: 1075494312055410729, (int)Discord.CreateFlags.Default);
 
-	// Use this for initialization
-	void Start()
-	{
-		discord = new Discord.Discord(461618159171141643, (System.UInt64)Discord.CreateFlags.Default);
-		var activityManager = discord.GetActivityManager();
-		var activity = new Discord.Activity
-		{
-			State = "In Main Menu",
-			Assets =
-			{
-				LargeImage = "discord",
-				LargeText = "The Main Hero of The Game"
-			}
-		};
-		activityManager.UpdateActivity(activity, (res) =>
-		{
-			if (res == Discord.Result.Ok)
-			{
-				Debug.LogError("Everything is fine!");
-			}
-		});
-	}
+        var activity = new Discord.Activity
+        {
+        State = state,
+        Assets =
+        {
+            LargeImage = imageKey,
+            LargeText = imageText,
+        },
+        Instance = instance,
+        };
 
-	// Update is called once per frame
-	void Update()
-	{
-		discord.RunCallbacks();
-	}
-}
+        _discord.GetActivityManager().UpdateActivity(activity, (result) =>
+        {
+            if (result == Discord.Result.Ok)
+            {
+                Debug.Log("Success!");
+            }
+            else
+            {
+                Debug.Log("Failed");
+            }
+        });
+    }
+    
+    public void Update()
+    {
+        _discord.RunCallbacks();
+    }
+
+    public void OnApplicationQuit()
+    {
+        _discord.Dispose();
+    }
+} 
