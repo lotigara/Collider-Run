bf39dc70d0f70c34d62e40f83bb242882fab3ac2
lotigara <105482918+lotigara@users.noreply.github.com>
2023-04-13 19:06:33 +0300

    Don't Know :neutral_face:

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -4,37 +4,10 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    public float _duration = .8f;
-    private Transform _cameraTransform;
-    private Vector3 _originalPosition;
- 
-    void Start()
-    {
-        _cameraTransform = GetComponent<Transform>();
-        _originalPosition = _cameraTransform.transform.position;
-    }
- 
+    public Animator cam;
+
     public void Shake()
     {
-        StartCoroutine(_Shake());
-    }
- 
-    IEnumerator _Shake()
-    {
- 
-        float x;
-        float y;
-        float timeLeft = Time.time;
- 
-        while ((timeLeft + _duration) > Time.time)
-        {
-            x = Random.Range(-0.3f, 0.3f);
-            y = Random.Range(-0.3f, 0.3f);
- 
-            _cameraTransform.position = new Vector3(x, y, _originalPosition.z); yield return new WaitForSeconds(0.025f);
-        }
- 
-        _cameraTransform.position = _originalPosition;
- 
+        cam.SetTrigger("shake");
     }
 }
