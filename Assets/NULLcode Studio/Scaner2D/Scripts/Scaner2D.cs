38d1bf44399756bf2558f0bfa5067125ce593a03
Strategne123 <lotigara@yandex.ru>
2023-05-05 17:40:10 +0300

    оу шит пам пам пам пам пам пам пам

--- Assets/NULLcode Studio/Scaner2D/Scripts/Scaner2D.cs
+++ Assets/NULLcode Studio/Scaner2D/Scripts/Scaner2D.cs
@@ -1,33 +1,27 @@
 ﻿using System.Collections;
-using System.Numerics;
 using UnityEngine;
 
-public class Scaner2D : MonoBehaviour
-{
+public class Scaner2D : MonoBehaviour {
+
 	[SerializeField] private LayerMask targetMask; // маски целей
 	[SerializeField] private LayerMask ignoreMask; // маски, которые нужно игнорировать (например, маска данного персонажа)
-	[SerializeField] private int rays = 3; // число лучей по формуле (N * 2) - 1, где N - данная переменная
-	[SerializeField] private float distance = 5; // длинна луча
+	[SerializeField] [Range(1, 300)] private int rays = 3; // число лучей по формуле (N * 2) - 1, где N - данная переменная
+	[SerializeField] [Range(1, 100)] private float distance = 5; // длинна луча
 	[SerializeField] [Range(0, 360)] private float angle = 20; // угол между лучами
 	[SerializeField] private Transform rayPoint; // объект, из которого выпускаются лучи
-    [SerializeField] private int speed; //скорость этого объекта
-    private UnityEngine.Vector2 movement; //разница расстояний игрока и этого объекта
-    private Transform player; //трансформ игрока
-    private Rigidbody2D rb; //риджидбади этого объекта
-    private int invert = 1;
+	private Transform player;
+	private Vector2 movement;
+	private Rigidbody2D rb;
+	public int speed = 5;
+	private int invert = 1;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-    private void MoveChar(UnityEngine.Vector2 direction)
-    {
-        rb.MovePosition((UnityEngine.Vector2)transform.position + (direction * speed * Time.deltaTime));
-    }
-
-    bool GetRay(UnityEngine.Vector2 dir)
+	bool GetRay(Vector2 dir)
 	{
 		bool result = false;
 
@@ -78,36 +72,42 @@ public class Scaner2D : MonoBehaviour
 
 			if(x != 0) 
 			{
-				if(GetRay(rayPoint.TransformDirection(new UnityEngine.Vector3(y,-x,0)))) hit = true;
+				if(GetRay(rayPoint.TransformDirection(new Vector3(y,-x,0)))) hit = true;
 			}
 
-			if(GetRay(rayPoint.TransformDirection(new UnityEngine.Vector3(y,x,0)))) hit = true;
+			if(GetRay(rayPoint.TransformDirection(new Vector3(y,x,0)))) hit = true;
 		}
 
 		return hit;
 	}
 
 	void Update()
-    {
-        if(Scan())
+	{
+		if(Scan())
 		{
-            UnityEngine.Vector3 direction = player.position - transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            direction.Normalize();
-            movement = direction;
-			transform.rotation = UnityEngine.Quaternion.Euler
+			Vector3 direction = player.position - transform.position;
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			direction.Normalize();
+			movement = direction;
+			transform.rotation = Quaternion.Euler
 			(
 				transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Mathf.Atan2
-			(
-			player.position.y - transform.position.y, player.position.x - transform.position.x
-			)
+				(
+					player.position.y - transform.position.y, player.position.x - transform.position.x
+				)
 				* Mathf.Rad2Deg
 			);
+
 			MoveChar(movement);
-        }
+		}
 		else
 		{
 			// Поиск цели...
 		}
+	}
+
+	private void MoveChar(Vector2 direction)
+	{
+		rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
 	}
 }
---
+++ Assets/Scripts/EnemyLoadDistance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLoadDistance : MonoBehaviour
+{
+    private ArrayList scaner2d;
+    public ArrayList enemies;
+    private void Start()
+    {
+        enemies.AddRange (GameObject.FindGameObjectsWithTag("Enemy"));
+        foreach (GameObject enemy in enemies)
+        {
+             scaner2d.Add(enemy.GetComponent<Scaner2D>());
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (enemies.Contains(col.gameObject))
+        {
+            col.gameObject.GetComponent<Scaner2D>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (enemies.Contains(col.gameObject))
+        {
+            col.gameObject.GetComponent<Scaner2D>().enabled = false;
+        }
+    }
+}
